10c71fd78000bb4cb6d1e3fa22ebab5b6dcb3bae
fariq <149790664+nuxxttt@users.noreply.github.com>
2024-09-18 19:17:51 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,20 @@
+    const questions = [
+        {
+            time: 68,
+            question: '',
+            options: ["a", "b", "c"],
+            correctAnswer: 0
+        },
+        {
+            time: 1430,
+            question: "",
+            options: ["a", "b", "c"],
+            correctAnswer: 1
+        },
+        {
+            time: 999999,
+            question: "What is the main character doing?",
+            options: ["Sleeping", "Eating", "Running", "Flying"],
+            correctAnswer: 2
+        }
+    ];
